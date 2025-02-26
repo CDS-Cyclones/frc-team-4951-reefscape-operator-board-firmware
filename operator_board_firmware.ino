6f37c61c89f634beff071613cbb407b9b58c9e4d
Andrii Bessarab <andriibessarab@gmail.com>
2025-02-26 15:33:03 -0500

    Update operator_board_firmware.ino

--- operator_board_firmware.ino
+++ operator_board_firmware.ino
@@ -1,29 +1,32 @@
 #include <light_CD74HC4067.h>
+#include <Joystick.h>
 
 CD74HC4067 mux(2, 3, 4, 5);
+CD74HC4067 mux2(15, 14, 16, 10);
 
 // Map pin id's to position around coral
-char channelToLetter[] = {'H', 'F', 'G', 'E', 'D', 'C', 'I', 'J', 'A', 'B', 'K', 'L'};
+int channelToButton[] = {7, 5, 6, 4, 3, 2, 8, 9, 0, 1, 10, 11, 1, 0, 5, 4, 2, 3};
+// int yellowChannelToButton = {1, 0, 5, 4, 2, 3};
 
 const int signal_pin = A0;                    // Pin Connected to Sig pin of CD74HC4067
 const int pressed_deadband = 870;             // Min analogue reading indicating button is pressed
 const unsigned long pressed_cooldown = 300;   // Cooldown in milliseconds
 const unsigned long debounce = 20;
 
-char april_tag_position = ' ';  // Variable to store the letter of the pressed button
+byte april_tag_position = -1;  // Variable to store the letter of the pressed button
 
 // Arrays to store button numbers for different types of buttons
 const int toggleButtons[] = {0, 1, 2, 3, 4, 5};       // Buttons that stay in when pressed
 const int momentaryButtons[] = {8, 9, 10, 11, 6, 7};  // Buttons that do not stay in when pressed
 
-class Button {
+class CoralButton {
  public:
   unsigned long last_pressed_time;
   bool state;
   bool is_toggle;
   bool last_value;
 
-  Button() {
+  CoralButton() {
     last_pressed_time = -1;
     state = false;
     is_toggle = false;
@@ -51,8 +54,50 @@ class Button {
   }
 };
 
+// Elevator class to handle elevator levels
+class Elevator {
+ public:
+  int levels[4];
+  
+  Elevator(int pin1, int pin2, int pin3, int pin4) {
+    levels[0] = pin1;
+    levels[1] = pin2;
+    levels[2] = pin3;
+    levels[3] = pin4;
+  }
+  
+  void checkLevels() {
+    for (int i = 0; i < 4; i++) {
+      if (digitalRead(levels[i]) == HIGH) {
+        Serial.println("Elevator level: " + String(i));
+        delay(debounce);  // debounce delay
+        resetLevels();
+        break;
+      }
+    }
+  }
+
+ private:
+  void resetLevels() {
+    for (int i = 0; i < 4; i++) {
+      if (digitalRead(levels[i]) == HIGH) {
+        // Wait for button to be released
+        while (digitalRead(levels[i]) == HIGH) {
+          delay(10);
+        }
+      }
+    }
+  }
+};
+
 // Create an array of Button objects for each coral position
-Button coral_buttons[12];
+CoralButton coral_buttons[12];
+
+// Create an elevator object with buttons on pins 6, 7, 8, 9
+Elevator elevator(6, 7, 8, 9);
+
+// Create joystick
+Joystick_ joystick;
 
 void setup() {
   Serial.begin(9600);
@@ -67,18 +112,63 @@ void setup() {
       }
     }
   }
+  
+  // Set elevator level pins as input
+  for (int i = 6; i < 10; i++) {
+    pinMode(i, INPUT);
+  }
+  
+  // Initialize Joystick Library
+	joystick.begin();
 }
 
 void loop() {
-  // Loop through channels [0, 11]
-  for (byte i = 0; i < 12; i++) {
+  // Loop through all coral buttons mux[0, 11]
+  for (byte i=0; i < 12; i++) {
     mux.channel(i);
     int val = analogRead(signal_pin);
     unsigned long current_time = millis();
 
     if (coral_buttons[i].shouldUpdatePose(val, current_time)) {
-      april_tag_position = channelToLetter[i];  // Update the AprilTagPosition with the corresponding letter
-      Serial.println("April tag position: " + String(april_tag_position));
+      april_tag_position = channelToButton[i];  // Update the AprilTagPosition with the corresponding button number
+      Serial.println(april_tag_position);
     }
   }
+  
+  // Set joystick values for coral position
+  for(byte but = 0; but < 12; but++) {
+    joystick.setButton(0, LOW);
+  }
+  if(april_tag_position != -1) {
+    joystick.setButton(april_tag_position, HIGH);
+  }
+
+  // for(byte i=12; i<14; i++) {
+  //   Serial.print(String(val) + "---");
+  // }
+
+  // The six yellow buttons
+  // for(byte i=0; i<7; i++) {
+  //   mux2.channel(i);
+  //   int val = analogRead(A0);
+  // }
+
+  // // The six swithces
+  // for(byte i=7; i<13; i++) {
+  //   mux2.channel(i);
+  //   int val = analogRead(A1);
+  //   Serial.print(String(val) + "---");
+  // }
+
+
+  // for (int i = 6; i < 10; i++) {
+  //   if (digitalRead(i) == LOW) {
+  //     Serial.println("Elevator level: " + String(i));
+  //   }
+  // }
+
+  //Serial.println(digitalRead(6));
+  
+  // Reset
+  april_tag_position = -1;
 }
